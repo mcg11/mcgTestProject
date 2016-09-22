ceec3d7d0c6d387176df2f98c6b8b2a5292c72b0
mcg11 <machunge2010@163.com>
2016-09-22 14:49:37 +0800

    dev 测试

--- TestProject2/src/main/java/mcg/comm/DevIam.java
+++ TestProject2/src/main/java/mcg/comm/DevIam.java
@@ -5,5 +5,6 @@ public class DevIam {
 	public static void main(String[] args) {
 		System.out.println("I am dev"); 
 		System.out.println("我又来");  
+		System.out.println("9-20-14:48 dev 修改");
 	}
 }
