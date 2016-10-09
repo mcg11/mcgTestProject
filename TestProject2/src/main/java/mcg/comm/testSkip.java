a4386a50df5bc3103eb36bdcddc69a21dba32aa4
mcg11 <machunge2010@163.com>
2016-10-09 16:10:18 +0800

    dev测试

--- TestProject2/src/main/java/mcg/comm/testSkip.java
+++ TestProject2/src/main/java/mcg/comm/testSkip.java
@@ -1,6 +1,9 @@
 package main.java.mcg.comm;
 
 public class testSkip {
+	public static void main(String[] args) {
+		System.out.println("我是dev");
+	}
 
 	public void testtest(){
 		System.out.println("我是test");
